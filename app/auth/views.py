26357b9eadaf52be2e6c0707b9f6ab77e676131d
Hesanjiang <805354657@qq.com>
2017-07-21 16:14:21 +0800

    flask Practice project

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import logout_user, login_user, login_required
 from . import auth
+from .. import db
+#from ..email import send_email
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -17,9 +19,26 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-@auth.route('/login')
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
