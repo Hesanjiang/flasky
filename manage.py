5e938c63dd1712589a580b52872291bbea94fbc4
Hesanjiang <805354657@qq.com>
2017-08-07 18:18:44 +0800

    my first commit

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from app import create_app, db
 from app.models import User, Role, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
-from flask import Flask
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
