0a3df83501dc681eb765fab349148b28848ca3ab
Hesanjiang <805354657@qq.com>
2017-09-13 20:42:58 +0800

    flask Practice project

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -10,6 +10,7 @@ def verify_password(email, password):
         g.current_user = AnonymousUser()
         return True
     user = User.query.filter_by(email=email).first()
+
     if not user:
         return False
     g.current_user = user
